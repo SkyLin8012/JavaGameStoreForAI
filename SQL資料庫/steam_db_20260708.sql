047aed86ff9bb27189596b3dee60001890b6e157
SkyLin8012 <99629426+SkyLin8012@users.noreply.github.com>
2026-07-08 07:20:21 +0800

    20260708-0720

---
+++ SQL資料庫/steam_db_20260708.sql
@@ -0,0 +1,279 @@
+CREATE DATABASE  IF NOT EXISTS `steam_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `steam_db`;
+-- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
+--
+-- Host: localhost    Database: steam_db
+-- ------------------------------------------------------
+-- Server version	9.7.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '78852612-52bb-11f1-8d1a-a0ad9fbae808:1-314';
+
+--
+-- Table structure for table `achievement`
+--
+
+DROP TABLE IF EXISTS `achievement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `achievement` (
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `requirement_text` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `achievement`
+--
+
+LOCK TABLES `achievement` WRITE;
+/*!40000 ALTER TABLE `achievement` DISABLE KEYS */;
+INSERT INTO `achievement` VALUES ('ach-admin','神之特權 (Administrator)','身為系統管理員 (ADMIN) 帳號登入平台。','平台全域','帳號角色為 ADMIN'),('ach-bird-15','黃金羽翼 (Golden Wings)','飛翔的小鳥單局飛過 15 根水管。','飛翔的小鳥','Flappy Bird 積分 >= 15'),('ach-brick-150','磚塊粉碎者 (Brick Destroyer)','打磚塊單局得分達到 150 分。','打磚塊','Brick Breaker 積分 >= 150'),('ach-brick-perfect','絕代神射手 (Perfect Aim)','打磚塊單局得分達到 300 分。','打磚塊','Brick Breaker 積分 >= 300'),('ach-collector','遊戲收藏家 (Game Collector)','在商店成功購買並擁有 3 款或以上遊戲。','平台全域','收藏庫遊戲數量 >= 3'),('ach-first-buy','蒸汽初體驗 (First Trade)','首次成功購買任意一款付費遊戲。','平台全域','完成任意 1 筆購買記錄'),('ach-gobang-win','棋聖下凡 (Gobang Master)','在五子棋中成功擊敗 AI 電腦。','五子棋','五子棋獲得 1 場勝場'),('ach-millionaire','石油大亨 (Steam Millionaire)','您的蒸汽錢包餘額超過 $1,000 元。','平台全域','錢包餘額 >= $1000'),('ach-pacman-2000','大胃王 (Pacman Feast)','小精靈單局得分達到 2000 分。','小精靈','Pacman 積分 >= 2000'),('ach-tetris-1000','方塊大師 (Tetris Master)','俄羅斯方塊單局得分達到 1000 分。','俄羅斯方塊','Tetris 積分 >= 1000');
+/*!40000 ALTER TABLE `achievement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cart_item`
+--
+
+DROP TABLE IF EXISTS `cart_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart_item` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `member_id` int NOT NULL,
+  `game_id` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_member_cart_game` (`member_id`,`game_id`),
+  KEY `game_id` (`game_id`),
+  CONSTRAINT `cart_item_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `cart_item_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart_item`
+--
+
+LOCK TABLES `cart_item` WRITE;
+/*!40000 ALTER TABLE `cart_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `game`
+--
+
+DROP TABLE IF EXISTS `game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `game` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `genre` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `thumbnail` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 0xF09F8EAE,
+  `banner_color` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'from-slate-700 to-slate-900',
+  `game_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `java_class_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game`
+--
+
+LOCK TABLES `game` WRITE;
+/*!40000 ALTER TABLE `game` DISABLE KEYS */;
+INSERT INTO `game` VALUES (1,'俄羅斯方塊 (Tetris)',50.00,'經典落體方塊遊戲，挑戰極速消除！','益智','img/tetris.png','from-cyan-700 to-blue-900','','com.steam.view.game.TetrisPanel'),(3,'經典2048 ',30.00,'遊戲的目標是在網格上讓相同的數值的方塊碰撞合併，最終取得數值為2048的方塊!','棋牌','img/G2048.png','from-amber-600 to-orange-800','','com.steam.view.game.Game2048'),(6,'貪吃蛇',100.00,'天吃蛇會一直吃蘋果，撞到牆或咬到自己會子動束。','益智','img/snake.png','from-slate-700 to-slate-900',NULL,'com.steam.view.game.SnakeGame');
+/*!40000 ALTER TABLE `game` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `game_score`
+--
+
+DROP TABLE IF EXISTS `game_score`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `game_score` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `member_id` int NOT NULL,
+  `game_id` int NOT NULL,
+  `score` int NOT NULL,
+  `recorded_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `member_id` (`member_id`),
+  KEY `game_id` (`game_id`),
+  CONSTRAINT `game_score_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `game_score_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=175 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game_score`
+--
+
+LOCK TABLES `game_score` WRITE;
+/*!40000 ALTER TABLE `game_score` DISABLE KEYS */;
+INSERT INTO `game_score` VALUES (172,1,1,0,'2026-07-07 22:48:00'),(173,1,6,10,'2026-07-07 22:48:46'),(174,1,3,544,'2026-07-07 22:53:44');
+/*!40000 ALTER TABLE `game_score` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `balance` decimal(10,2) DEFAULT '1000.00',
+  `role` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'USER',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (1,'admin','admin123','admin@steam.com','系統管理員',9799.00,'ADMIN','2026-07-01 06:43:16'),(2,'user1','user123','user1@gmail.com','玩家一號',500.00,'USER','2026-07-01 06:43:16'),(3,'user2','user123','user2@gmail.com','玩家二號',100.00,'USER','2026-07-01 06:43:16');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchase`
+--
+
+DROP TABLE IF EXISTS `purchase`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `purchase` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `member_id` int NOT NULL,
+  `game_id` int NOT NULL,
+  `purchase_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `transaction_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `price_at_purchase` decimal(10,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_member_game` (`member_id`,`game_id`),
+  KEY `game_id` (`game_id`),
+  KEY `transaction_id` (`transaction_id`),
+  CONSTRAINT `purchase_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `purchase_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `purchase_ibfk_3` FOREIGN KEY (`transaction_id`) REFERENCES `purchase_order` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchase`
+--
+
+LOCK TABLES `purchase` WRITE;
+/*!40000 ALTER TABLE `purchase` DISABLE KEYS */;
+INSERT INTO `purchase` VALUES (1,2,3,'2026-07-01 06:43:16',NULL,30.00),(3,1,3,'2026-07-01 07:12:18',NULL,0.00),(7,1,6,'2026-07-02 11:12:39',NULL,0.00),(10,1,1,'2026-07-07 21:18:20',NULL,0.00);
+/*!40000 ALTER TABLE `purchase` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchase_order`
+--
+
+DROP TABLE IF EXISTS `purchase_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `purchase_order` (
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `member_id` int NOT NULL,
+  `total_price` decimal(10,2) NOT NULL,
+  `purchase_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `purchase_order_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchase_order`
+--
+
+LOCK TABLES `purchase_order` WRITE;
+/*!40000 ALTER TABLE `purchase_order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `purchase_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `unlocked_achievement`
+--
+
+DROP TABLE IF EXISTS `unlocked_achievement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `unlocked_achievement` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `member_id` int NOT NULL,
+  `achievement_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `unlocked_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_member_achievement` (`member_id`,`achievement_id`),
+  KEY `achievement_id` (`achievement_id`),
+  CONSTRAINT `unlocked_achievement_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `unlocked_achievement_ibfk_2` FOREIGN KEY (`achievement_id`) REFERENCES `achievement` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1024 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `unlocked_achievement`
+--
+
+LOCK TABLES `unlocked_achievement` WRITE;
+/*!40000 ALTER TABLE `unlocked_achievement` DISABLE KEYS */;
+INSERT INTO `unlocked_achievement` VALUES (1,1,'ach-millionaire','2026-07-01 06:55:32'),(2,1,'ach-first-buy','2026-07-01 06:55:32'),(14,1,'ach-gobang-win','2026-07-01 07:12:42'),(16,1,'ach-collector','2026-07-01 07:49:43'),(37,1,'ach-admin','2026-07-02 05:08:04'),(38,1,'ach-bird-15','2026-07-02 05:08:04'),(40,1,'ach-tetris-1000','2026-07-02 05:08:04'),(41,1,'ach-pacman-2000','2026-07-02 05:08:04'),(42,1,'ach-brick-150','2026-07-02 05:08:04');
+/*!40000 ALTER TABLE `unlocked_achievement` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-07-08  7:07:45
